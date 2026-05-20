e5dfff009569a8c06c6f8cf5d3796b4a1879054d
Aqslav <antonio.mudryj@gmail.com>
2026-05-20 15:22:12 +0300

    added category list view

--- recipe/views.py
+++ recipe/views.py
@@ -5,3 +5,12 @@ def main_view(request):
     latest_recipes = Recipe.objects.order_by('-id')[:5] 
     context = {'recipes': latest_recipes}
     return render(request, 'main.html', context)
+
+from django.shortcuts import render
+from django.db.models import Count
+from .models import Category
+
+def category_list_view(request):
+    categories_with_count = Category.objects.annotate(recipes_count=Count('recipe'))
+    context = {'categories': categories_with_count}
+    return render(request, 'category_list.html', context)
