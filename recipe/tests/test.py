2b730c13044e25a9adb76192c84a1fdb9f2d594c
Aqslav <antonio.mudryj@gmail.com>
2026-05-20 15:28:38 +0300

    added test method

--- recipe/tests/test.py
+++ recipe/tests/test.py
@@ -7,3 +7,9 @@ class RecipeNewViewsTestCase(TestCase):
         self.category = Category.objects.create(name="Soup")
         for i in range(7):
             Recipe.objects.create(title=f"Recipe {i}", category=self.category)
+
+    def test_main_view_returns_latest_five(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertEqual(len(response.context['recipes']), 5)
