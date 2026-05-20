fa964c4dbf79e2d4ecf6ea7d27ce3f4211a9525a
Aqslav <antonio.mudryj@gmail.com>
2026-05-20 16:07:31 +0300

    style

--- recipe/tests.py
+++ recipe/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Recipe, Category
 
+
 class RecipeNewViewsTestCase(TestCase):
     def setUp(self):
         self.category = Category.objects.create(name="Soup")
@@ -15,9 +16,9 @@ class RecipeNewViewsTestCase(TestCase):
         self.assertEqual(len(response.context['recipes']), 5)
 
     def test_category_list_view_counts(self):
-            response = self.client.get(reverse('category_list'))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'category_list.html')
-            categories_in_context = response.context['categories']
-            test_category = categories_in_context.get(id=self.category.id)
-            self.assertEqual(test_category.recipes_count, 7)
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'category_list.html')
+        categories_in_context = response.context['categories']
+        test_category = categories_in_context.get(id=self.category.id)
+        self.assertEqual(test_category.recipes_count, 7)
--- recipe/views.py
+++ recipe/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from .models import Recipe
+from .models import Recipe, Category
+from django.db.models import Count
+
 
 def main_view(request):
-    latest_recipes = Recipe.objects.order_by('-id')[:5] 
+    latest_recipes = Recipe.objects.order_by('-id')[:5]
     context = {'recipes': latest_recipes}
     return render(request, 'main.html', context)
 
-from django.shortcuts import render
-from django.db.models import Count
-from .models import Category
 
 def category_list_view(request):
     categories_with_count = Category.objects.annotate(recipes_count=Count('categories'))
